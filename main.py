c1169ee9f4035390710ccf870a3e870ec9c21932
Aze543 <121960248+Aze543@users.noreply.github.com>
2025-04-09 01:26:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+import img2pdf # type: ignore
+import requests
+from typing import NoReturn
+from bs4 import BeautifulSoup
+import re
+import sys
+import time
+import threading
+
+on = True
+la = False
+logo = """
+.##..##..##..##..######..##..##..######...####...######.                        
+.###.##..##..##..##......###.##....##....##..##....##...                        
+.##.###..######..####....##.###....##....######....##...                        
+.##..##..##..##..##......##..##....##....##..##....##...                        
+.##..##..##..##..######..##..##....##....##..##..######.                        
+........................................................                        
+.#####....####...##..##..######..######..##..##..........######...####..........
+.##..##..##..##..##..##......##....##....###.##............##....##..##.........
+.##..##..##..##..##..##......##....##....##.###..######....##....##..##..######.
+.##..##..##..##..##..##..##..##....##....##..##............##....##..##.........
+.#####....####....####....####...######..##..##............##.....####..........
+................................................................................
+.#####...#####...######..........##...##...####...##..##..######..#####..       
+.##..##..##..##..##..............###.###..##..##..##.##...##......##..##.       
+.#####...##..##..####............##.#.##..######..####....####....#####..       
+.##......##..##..##..............##...##..##..##..##.##...##......##..##.       
+.##......#####...##..............##...##..##..##..##..##..######..##..##.       
+.........................................................................         
+"""
+
+def loading_animation():
+    global la
+    spinner = ['|', '/', '-', '\\']
+    while not la:
+        for frame in spinner:
+            sys.stdout.write(f'\rLet the magic work... {frame}')
+            sys.stdout.flush()
+            time.sleep(0.1)
+            if la:  
+                sys.stdout.flush()
+                break
+    sys.stdout.write('\rLet the magic work...   ')  
+    sys.stdout.flush()
+
+def get_images(raw: list, pages: int) -> list:
+    imgs= []
+    page = 1
+    for item in raw:
+        img_code, f_type = re.search(r'/(\d+)', item['data-src']).group(), re.search(r'\.([a-zA-Z0-9]+)$', item['data-src']).group()
+        imgs.append(f'https://i3.nhentai.net/galleries{img_code}/{page}{f_type}')
+        page += 1
+        if page > pages:
+            return imgs
+        
+def compile_images(urls: list, name: str) -> NoReturn:
+    raw_images = []
+    for url in urls:
+        response = requests.get(url)
+        raw_images.append(response.content)
+    with open(f"{name}.pdf", "wb") as file:
+        file.write(img2pdf.convert(raw_images))
+
+def main(on):
+    global la
+    print(f"{logo}\n\n")
+    while on:
+        try: 
+            code = int(input("Type the code(IYKYK): "))
+        except Exception as e:
+            print("\nThe code needs to be numbers not letters (Ex: 177013) :) \n")
+            continue
+        url = f'https://nhentai.net/g/{code}/'
+        response = requests.get(url)
+        try: 
+            soup = BeautifulSoup(response.text, "html.parser")
+            pages = len(soup.find_all(class_='gallerythumb'))
+            name = soup.find(class_='pretty').text
+            raw_data = soup.find_all(class_='lazyload')
+        except Exception as e:
+            print("\n[ERROR]: The code cannot be found in the website.\n")
+            continue
+        function_thread = threading.Thread(target=loading_animation)
+        function_thread.start()
+        url_images = get_images(raw_data, pages)
+        compile_images(url_images, name)
+        la = True
+        function_thread.join()
+        print(f"\nThe Operation was success, the file was saved into '{name}'")
+        user_response = input("\nstill need then app to create another pdf(Y or N)?\n").lower()
+        if not user_response == 'y' or user_response == 'yes':
+            print("Exiting.....")
+            on = False
+
+if __name__ == "__main__":
+    main(on)
---
+++ readme.txt
@@ -0,0 +1,65 @@
+
+---------------------------------------------------------------------------------
+
+I use MacOS, so I might know how to run it on windows but i'll try my best
+
+---------------------------------------------------------------------------------
+
+INSTURCTIONS:
+
+---------------------------------------------------------------------------------
+
+1. install python to your system, look in the internet how
+
+to check if it works
+
+windows: open cmd >> type python >> hit enter
+
+mac: open terminal >> type python3 >> hit enter
+
+it should open the python
+
+---------------------------------------------------------------------------------
+
+2. install the requirements in 'requirements.txt'
+
+in the cmd or terminal, go into the directory of the file where
+the main.py and requirements.txt lives
+
+directory means folder
+
+to check if you're now in the directory
+
+windows: the command is dir to see the list of files in the directory
+
+mac: type ls
+
+finally, type this in the cmd or terminal: pip install -r 'requirements.txt'
+
+it should download the packages you need to make the script works
+
+---------------------------------------------------------------------------------
+
+3. last is run the program by typing:
+
+windows: python main.py
+
+mac: python3 main.py
+
+if you're greeted with the logo "NHENTAI DOUJIN-TO-PDF MAKER"
+in the cmd or terminal, you've successfully install and run the script 
+into your computer, Enjoy!
+
+---------------------------------------------------------------------------------
+
+HOW TO USE THE SCRIPT:
+
+---------------------------------------------------------------------------------
+
+type the code of the nhentai doujin you want, then let the magic work
+
+the output will be in the same directory
+
+Dev Note: This Script only works on nhentai.net
+
+---------------------------------------------------------------------------------
